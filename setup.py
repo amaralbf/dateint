7939429d08d1d16633f5f7df896329409172a486
Bruno Amaral <amaralbf@gmail.com>
2021-03-04 16:16:21 -0300

    Move build tool to setuptools

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dateint',
+    version='0.1.0',
+    description='',
+    long_description=None,
+    author='Bruno Amaral',
+    author_email='amaralbf@gmail.com',
+    packages=['dateint'],
+    python_requires='>=3.7,<4.0',
+)
